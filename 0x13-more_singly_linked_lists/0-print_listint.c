359eb49b269a440e4854d0d58097be9a1210a60c
ManLikeMarko <cramohchalo@gmail.com>
2023-08-28 22:50:39 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count_node = 0;
 
-	while(h ! = NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", h -> n);
-		h = h-> next;
+		printf("%d\n", h->n);
+		h = h->next;
 		count_node++;
 	}
-	return(count_node);
+	return (count_node);
 }
