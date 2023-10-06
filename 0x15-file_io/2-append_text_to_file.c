3de0eeae6ade7d6a2a0a9467e362d1adca43f988
root <cramohchalo@gmail.com>
2023-10-06 13:47:17 -0700

    appnd

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,6 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (text_content = NULL;)
 			for (len = 0; text_content[len];)
 				len++;
 	}
