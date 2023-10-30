fa8845bc54fa760b0a5b1aeb298925190f2fd7c7
ManLikeMarko <cramohchalo@gmail.com>
2023-10-30 18:02:20 +0000

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,54 +1,30 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile - read txt file prnt to STDOUT
- * @filename: text fle being raed
- * @letters: number of letters to be read
+ * read_textfile - A function that reads a textfile then prints it to POSIX
+ * std output
+ * @filename: the file name
+ * @letters: the number of bytes that are to be read
  *
- * Return: actual no. of bytes read and printed
- * 0 if fxn feils
- *
+ * Return: the number of bytes that are printed
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, ret;
-	char *buf;
+	int file_data;
+	ssize_t bytes;
+	char buf[BUF_SIZE * 8];
 
-	if (filename == NULL)
+	if (!filename || !letters)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	file_data = open(filename, O_RDONLY);
+	if (file_data == -1)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		free(buf);
-		return (0);
-	}
-	ret = read(fd, buf, letters);
-	if (ret == -1)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
-	if (write(STDOUT_FILENO, buf, ret) != ret)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
-	if (write(STDOUT_FILENO, buf, ret) != ret)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
-	free(buf);
-	close(fd);
-	return (ret);
+
+	bytes = read(file_data, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
+	close(file_data);
+
+	return (bytes);
 }
-
